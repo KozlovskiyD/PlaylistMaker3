43451db59b535aac8052e6576647cbe3ba3569e0
alferac@inbox.ru <dmitr-nik.k@yandex.ru>
2023-03-11 11:49:11 +0500

    Init commit Toast

--- app/src/main/java/com/practicum/playlistmaker3/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker3/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.practicum.playlistmaker3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val buttonSearch = findViewById<Button>(R.id.search_button)
+        val buttonSearchClick: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажали на кнопку < Поиск >", Toast.LENGTH_SHORT).show()
+            }
+        }
+        buttonSearch.setOnClickListener(buttonSearchClick)
     }
 }
