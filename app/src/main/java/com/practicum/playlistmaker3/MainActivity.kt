dda1632f2efbef34c123b7ad22a9a8339a8e3060
alferac@inbox.ru <dmitr-nik.k@yandex.ru>
2023-03-11 19:11:57 +0500

    Init commit Toast all Домашная работа часть 3

--- app/src/main/java/com/practicum/playlistmaker3/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker3/MainActivity.kt
@@ -11,22 +11,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val buttonSearch = findViewById<Button>(R.id.search_button)
         val buttonSearchClick: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на кнопку < Поиск >", Toast.LENGTH_SHORT).show()
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchIntent)
             }
         }
         buttonSearch.setOnClickListener(buttonSearchClick)
 
         val buttonMedia = findViewById<Button>(R.id.media_button)
         buttonMedia.setOnClickListener {
-            Toast.makeText(this, "Нажали на кнопку < Медиатека >", Toast.LENGTH_SHORT).show()
+            val mediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(mediaIntent)
         }
 
         val buttonSetting = findViewById<Button>(R.id.setting_button)
         buttonSetting.setOnClickListener {
-            Toast.makeText(this, "Нажали на кнопру< Настройки >", Toast.LENGTH_SHORT).show()
+            val settingIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingIntent)
         }
     }
 }
