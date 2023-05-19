df3e3314819b01a0849405c27fbcc54a40d86d38
alferac@inbox.ru <dmitr-nik.k@yandex.ru>
2023-05-20 01:39:43 +0500

    Init commit Toast all Домашная работа часть 3.5 Домашная работа спринт 9.1 Домашняя работа спринт 10.1 домашняя работа спринт 12

--- app/src/main/java/com/practicum/playlistmaker3/SearchHistory.kt
+++ app/src/main/java/com/practicum/playlistmaker3/SearchHistory.kt
@@ -4,25 +4,36 @@ import android.content.SharedPreferences
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.practicum.playlistmaker3.SearchActivity.Companion.KEY
+import com.practicum.playlistmaker3.SearchActivity.Companion.KEY_TRACK
 
-class SearchHistory(
-    private val sharedPrefs: SharedPreferences,
-    private var listSave: MutableList<Track>
-) {
-    fun saveHistory(saveTrack: Track) {
-        val loadGson = sharedPrefs.getString(KEY, "")
-        if (loadGson != "") listSave =
-            Gson().fromJson(loadGson, object : TypeToken<ArrayList<Track>>() {}.type)
-        var indexTrack = 10
-        for (item in listSave) {
-            if (item.trackId == saveTrack.trackId) indexTrack = (listSave.indexOf(item))
+class SearchHistory(private val sharedPrefs: SharedPreferences) {
+
+    private var listSave = mutableListOf<Track>()
+
+    fun saveHistory(saveTrack: Track, current: Boolean) {
+        if (current) {
+            val loadGson = sharedPrefs.getString(KEY, "")
+            if (loadGson != "") listSave =
+                Gson().fromJson(loadGson, object : TypeToken<ArrayList<Track>>() {}.type)
+            var indexTrack = 10
+            for (item in listSave) {
+                if (item.trackId == saveTrack.trackId) indexTrack = (listSave.indexOf(item))
+            }
+            if (indexTrack < 10) listSave.removeAt(indexTrack)
+            if (listSave.size >= 10) listSave.removeAt(index = 9)
+            listSave.add(0, saveTrack)
+            val json = Gson().toJson(listSave)
+            sharedPrefs.edit()
+                .putString(KEY, json.toString())
+                .apply()
         }
-        if (indexTrack < 10) listSave.removeAt(indexTrack)
-        if (listSave.size >= 10) listSave.removeAt(index = 9)
-        listSave.add(0, saveTrack)
-        val json = Gson().toJson(listSave)
+        saveTrack(saveTrack)
+    }
+
+    private fun saveTrack(track: Track) {
+        val json = Gson().toJson(track)
         sharedPrefs.edit()
-            .putString(KEY, json.toString())
+            .putString(KEY_TRACK, json.toString())
             .apply()
     }
 }
